b73b40a46a37a97c0b8d419641c450dae2c3e630
vagrant <vagrant@homestead>
2015-11-24 16:34:37 +0000

    adding content

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -12,62 +12,166 @@
     <title>Bakery - Too Pixel</title>
 
     <link href="css/app.css" rel="stylesheet">
-
 </head>
 
 <body>
 
-    <!-- Navigation -->
-    <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
-        <div class="container">
-            <!-- Brand and toggle get grouped for better mobile display -->
-            <div class="navbar-header">
-                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-                    <span class="sr-only">Toggle navigation</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
-                <a class="navbar-brand" href="#">Start Bootstrap</a>
-            </div>
-            <!-- Collect the nav links, forms, and other content for toggling -->
-            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-                <ul class="nav navbar-nav">
-                    <li>
-                        <a href="#">About</a>
-                    </li>
-                    <li>
-                        <a href="#">Services</a>
-                    </li>
-                    <li>
-                        <a href="#">Contact</a>
-                    </li>
-                </ul>
-            </div>
-            <!-- /.navbar-collapse -->
-        </div>
-        <!-- /.container -->
-    </nav>
-
-    <!-- Page Content -->
+<!-- Navigation -->
+<nav class="navbar navbar-inverse navbar-fixed-top" role="navigation" style="display: none">
     <div class="container">
+        <!-- Brand and toggle get grouped for better mobile display -->
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle" data-toggle="collapse"
+                    data-target="#bs-example-navbar-collapse-1">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="#">Start Bootstrap</a>
+        </div>
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+            <ul class="nav navbar-nav">
+                <li>
+                    <a href="#">About</a>
+                </li>
+                <li>
+                    <a href="#">Services</a>
+                </li>
+                <li>
+                    <a href="#">Contact</a>
+                </li>
+            </ul>
+        </div>
+        <!-- /.navbar-collapse -->
+    </div>
+    <!-- /.container -->
+</nav>
 
+<!-- Page Content -->
+<div class="container">
+    <div class="row">
+        <div class="col-md-2 no-padding">
+            <img src="/images/toopixel-ibread-logo.jpg" title="TooPixel iBread Logo" alt="iBread"/>
+        </div>
+        <div class="col-md-10"></div>
+    </div>
+    <div class="row top-margin">
+        <div class="col-md-5 no-padding">
+            <img src="/images/toopixel-phone-bread-dancer.jpg" title="TooPixel featured image"
+                 alt="TooPixel Featured image"/>
+        </div>
+        <div class="col-md-7 no-padding">
+            <h1>Remarkable bread slices</br>right from the start</h1>
+        </div>
+        <div class="col-md-7 no-padding top-margin-small">
+            <p>Tired of fat and uneven brad</p>
+        </div>
+        <div class="col-md-3 no-padding top-margin-small">
+            <p>Don't worry my chum, all your worries are coming to an endsdffsfskfhjsk shfksf hsfkjhk jhsjfsk hskjfhskjk
+                sjhfk jhfkj .</p>
+        </div>
+        <div class="col-md-4 no-padding">
+            <p>No matter what bread it is, no matter sdkksfshjkshfjkhsfkskfhskfhksfhkshfksh</p>
+        </div>
+        <div class="col-md-7 no-padding top-margin-small">
+            <img src="/images/toopixel-available-on-appstore.gif" title="Available on AppStore"
+                 alt="Available on AppStore"/>
+        </div>
+    </div>
+</div>
+<div class="container-fluid container-middle top-margin-large bottom-padding-large">
+    <div class="container">
+        <div class="row ">
+            <div class="col-md-12">
+                <div class="header">
+                    <h2>The best thing that could hapen to a sliced bread</h2>
+                    <div class="text top-margin-medium">
+                        <p>Just take a look lsfjlkfjlkjflks jlskjflksj lkfsjlfkjs lkjsdlkjf lskjdflsjf lskdfjls
+                            jlskjdflskjfl</p>
+                        <p>Just take a look lsfjlkfjlkjflks jlskjflksj lkfsjlfkjs lkjsdlkjf lskjdflsjf lskdfjls
+                            jlskjdflskjfl</p>
+                    </div>
+                </div>
+            </div>
+        </div>
         <div class="row">
-            <div class="col-lg-12 text-center">
-                <h1>A Bootstrap Starter Template</h1>
-                <p class="lead">Complete with pre-defined file paths that you won't have to change!</p>
-                <ul class="list-unstyled">
-                    <li>Bootstrap v3.3.1</li>
-                    <li>jQuery v1.11.1</li>
-                </ul>
+            <div class="col-md-3 no-padding top-margin-small">
+                <a href="#">
+                    <img src="/images/toopixel-sliced-bread-1.gif" title="TooPixel Sliced Bread 1"
+                         alt="TooPixel Sliced Bread 1"/>
+                </a>
+            </div>
+            <div class="col-md-3 no-padding top-margin-small">
+                <a href="#">
+                    <img src="/images/toopixel-sliced-bread-2.gif" title="TooPixel Sliced Bread 2"
+                         alt="TooPixel Sliced Bread 2"/>
+                </a>
+            </div>
+            <div class="col-md-3 no-padding top-margin-small">
+                <a href="#">
+                    <img src="/images/toopixel-sliced-bread-3.gif" title="TooPixel Sliced Bread 3"
+                         alt="TooPixel Sliced Bread 3"/>
+                </a>
+            </div>
+            <div class="col-md-3 no-padding top-margin-small">
+                <a href="#">
+                    <img src="/images/toopixel-sliced-bread-4.gif" title="TooPixel Sliced Bread 4"
+                         alt="TooPixel Sliced Bread 4"/>
+                </a>
+            </div>
+            <div class="col-md-3 no-padding top-margin-small">
+                <a href="#">
+                    <img src="/images/toopixel-sliced-bread-5.gif" title="TooPixel Sliced Bread 5"
+                         alt="TooPixel Sliced Bread 5"/>
+                </a>
+            </div>
+            <div class="col-md-3 no-padding top-margin-small">
+                <a href="#">
+                    <img src="/images/toopixel-sliced-bread-6.gif" title="TooPixel Sliced Bread 6"
+                         alt="TooPixel Sliced Bread 6"/>
+                </a>
+            </div>
+            <div class="col-md-3 no-padding top-margin-small">
+                <a href="#">
+                    <img src="/images/toopixel-sliced-bread-7.gif" title="TooPixel Sliced Bread 7"
+                         alt="TooPixel Sliced Bread 7"/>
+                </a>
+            </div>
+            <div class="col-md-3 no-padding top-margin-small">
+                <a href="#">
+                    <img src="/images/toopixel-sliced-bread-8.gif" title="TooPixel Sliced Bread 8"
+                         alt="TooPixel Sliced Bread 8"/>
+                </a>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="container-fluid container-bottom bottom-padding-large">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="logo">
+                    <img src="/images/toopixel-footer-ibread-logo.gif" title="iBread Logo Dark"
+                         alt="iBread logo dark"/>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-12">
+                <footer class="top-margin-large">
+                    <h3>Thank you for your time</h3>
+                    <span>Or you can contact us at: <a href="mailto:info@ibread.com" title="Contact Us">info@ibread.com</a></span>
+                </footer>
             </div>
         </div>
         <!-- /.row -->
-
     </div>
-    <!-- /.container -->
+</div>
+<!-- /.container -->
 
-    <script src="js/all.js"></script>
+<script src="js/all.js"></script>
 </body>
 
 </html>
